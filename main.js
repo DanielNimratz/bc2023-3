fa62369ad688cf2785036eebc3ac53afca69a700
Daniel <34036638+DanielNimratz@users.noreply.github.com>
2023-10-09 17:24:24 +0300

    update-tabultaion

--- main.js
+++ main.js
@@ -1,42 +1,39 @@
-const fs = require('fs');
+const fs = require("fs");
 
 function main() {
-    fs.readFile('data.json', 'utf-8', (err, data) => {
-        if (err) {
-            console.error("error reading this file: data.json", err);
-        }
+  fs.readFile("data.json", "utf-8", (err, data) => {
+    if (err) {
+      console.error("error reading this file: data.json", err);
+    }
 
-        try {
-            const jsonObj = JSON.parse(data);
-            let minValue = Infinity;
-            let minKey = null;
-            for (const key in jsonObj) {
-                if (jsonObj.hasOwnProperty(key)) {
-                  const value = jsonObj[key].value;
-                  if (value < minValue) {
-                    minValue = value;
-                    minKey = key;
-                  }
-                }
-              } 
-            if (minKey !== null) {
-            let valueName = jsonObj[minKey].txt;
-            let lowestValueStr = valueName + ":" + minValue;
-            fs.writeFile('output.txt', lowestValueStr, 'utf8', (err) => {
-                if (err) {
-                  console.error('Error writing to file:', err);
-                } 
-              });
-            } else {
-              console.log('No elements found in the JSON.');
-            }
+    try {
+      const jsonObj = JSON.parse(data);
+      let minValue = Infinity;
+      let minKey = null;
+      for (const key in jsonObj) {
+        if (jsonObj.hasOwnProperty(key)) {
+          const value = jsonObj[key].value;
+          if (value < minValue) {
+            minValue = value;
+            minKey = key;
+          }
         }
-    
-
-    catch (error) {
-            console.error('Error parsing data.JSON:', error);}
+      }
+      if (minKey !== null) {
+        let valueName = jsonObj[minKey].txt;
+        let lowestValueStr = valueName + ":" + minValue;
+        fs.writeFile("output.txt", lowestValueStr, "utf8", (err) => {
+          if (err) {
+            console.error("Error writing to file:", err);
+          }
         });
+      } else {
+        console.log("No elements found in the JSON.");
+      }
+    } catch (error) {
+      console.error("Error parsing data.JSON:", error);
+    }
+  });
 }
 
-main('data.json');
-
+main("data.json");
