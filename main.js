f3fe1e61c3209a10c563d2def446a5014072234e
unknown <DanieNimratz@daniel.nimratz@gmail.com>
2023-10-09 09:13:39 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+
+function main() {
+    fs.readFile('data.json', 'utf-8', (err, data) => {
+        if (err) {
+            console.error("error reading this file: data.json", err);
+        }
+
+        try {
+            const jsonObj = JSON.parse(data);
+            let minValue = Infinity;
+            let minKey = null;
+            for (const key in jsonObj) {
+                if (jsonObj.hasOwnProperty(key)) {
+                  const value = jsonObj[key].value;
+                  if (value < minValue) {
+                    minValue = value;
+                    minKey = key;
+                  }
+                }
+              } 
+            if (minKey !== null) {
+            let valueName = jsonObj[minKey].txt;
+            let minValue = jsonObj[minKey].value;
+            let lowestValueStr = valueName + ":" + minValue;
+            fs.writeFile('text.txt', lowestValueStr, 'utf8', (err) => {
+                if (err) {
+                  console.error('Error writing to file:', err);
+                } else {
+                  console.log(`Updated file with information about the element with the lowest value: ${minKey}: ${minValue}`);
+                }
+              });
+            } else {
+              console.log('No elements found in the JSON.');
+            }
+        }
+    
+
+    catch (error) {
+            console.error('Error parsing data.JSON:', error);}
+        });
+}
+
+main('data.json');
+
